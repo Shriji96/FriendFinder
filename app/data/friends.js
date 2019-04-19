e8fdadce7cd7316b1546aef855d989d24aa277ea
Shriji Shah <Shriji_96@yahoo.com>
2019-04-19 11:04:24 -0700

    updated friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friendsArray = [
+    {
+        name:"Shriji",
+        photo:"https://www.irishtimes.com/polopoly_fs/1.3135544.1498571483!/image/image.jpg_gen/derivatives/box_620_330/image.jpg",
+        scores:[
+          3,
+          4,
+          5,
+          4,
+          4,
+          3,
+          5,
+          2,
+          5,
+          4
+        ]
+
+
+    }
+];
+
+module.exports = friendsArray;
